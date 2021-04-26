b8d792d43a925e7e3134931ff892679f89f10b07
tech developer <80821792+shield44@users.noreply.github.com>
2021-04-26 16:20:47 +0530

    Add files via upload

---
+++ lineage_olives.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2019-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from olivewood device
+$(call inherit-product, device/xiaomi/olives/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_olives
+PRODUCT_DEVICE := olives
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 8 Series
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
